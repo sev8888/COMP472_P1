bd3c7c7ffbbfa84b11f64ce8254a01573f409239
mint47 <dor012299@gmail.com>
2021-09-26 01:59:15 -0400

    added steps 3 and 4 to main.py

--- main.py
+++ main.py
@@ -1,13 +1,31 @@
 import os
+from sklearn import datasets
+from sklearn.feature_extraction.text import CountVectorizer
+import sklearn.model_selection
+import sklearn.naive_bayes as NB
 
-#if __name__ == '__main__':
-cwd = os.getcwd()
-print(cwd+os.sep+"BBC")
-#"""
-for subdir, dirs, files in os.walk(cwd):
-    for filename in files:
-        filepath = subdir + os.sep + filename
+#finding BBC subdir in local directory (BBC folder needs to be in project folder COMP472_P1)
+for subdir, dirs, files in os.walk(os.curdir):
+    for dir in dirs:
+        if dir == "BBC":
+            bbcpath = os.path.join(subdir,dir)
+            #print(bbcpath)
 
-        if filepath.endswith(".txt"):
-            print(filepath)
-#"""
+#step 2
+#TODO insert step 2 code here
+
+#step 3
+corpus = datasets.load_files(container_path = bbcpath, description = "BBC dataset", encoding = 'latin1', shuffle=False)
+# returns a 'Bunch' with attributes data[], target[], target_names[], DESCR, filenames[]
+#           data[] = content of file as one string
+#         target[] = class of the file as int (index for target_name)
+#   target_names[] = name of class
+#            DESCR = description of dataset
+#      filenames[] = file names
+# -> if we need to randomize data, set shuffle to True(default)
+
+#step 4 TODO adapt for step 6
+vectorizerinator = CountVectorizer()
+X = vectorizerinator.fit_transform(corpus.data)
+print(vectorizerinator.get_feature_names())
+print(X.toarray())
