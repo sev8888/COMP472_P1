9393c00e3159195ad3926e219788781852468b2d
Minh-Tam <dor012299@gmail.com>
2021-09-27 13:14:44 -0400

    Q2 part 1
    added extraction of file count and categorie names for Q2
    removed BBC.zip from .gitignore

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from sklearn import datasets
 from sklearn.feature_extraction.text import CountVectorizer
 import sklearn.model_selection
 import sklearn.naive_bayes as NB
+import glob
 
 #finding BBC subdir in local directory (BBC folder needs to be in project folder COMP472_P1)
 for subdir, dirs, files in os.walk(os.curdir):
@@ -13,6 +14,19 @@ for subdir, dirs, files in os.walk(os.curdir):
 
 #step 2
 #TODO insert step 2 code here
+categories = list()
+fcount = [0]
+for path in glob.glob(bbcpath+'\*'):
+    if os.path.isdir(path) and path != bbcpath: #ignore source folder BBC
+        categories.append(os.path.basename(path))
+        fcount[0] = 0
+        for file in glob.glob(path+'\*.txt'):
+            fcount[0] = fcount[0] + 1 #fcount[0] serves as our counter
+        fcount.append(fcount[0])
+fcount.pop(0) #remove counter fcount[0]
+print(categories)
+print(fcount)
+
 
 #step 3
 corpus = datasets.load_files(container_path = bbcpath, description = "BBC dataset", encoding = 'latin1', shuffle=False)
@@ -25,7 +39,7 @@ corpus = datasets.load_files(container_path = bbcpath, description = "BBC datase
 # -> if we need to randomize data, set shuffle to True(default)
 
 #step 4 TODO adapt for step 6
-vectorizerinator = CountVectorizer()
+"""vectorizerinator = CountVectorizer()
 X = vectorizerinator.fit_transform(corpus.data)
 print(vectorizerinator.get_feature_names())
-print(X.toarray())
+print(X.toarray())"""
