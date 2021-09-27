0930a25f65750a596a6e3c2361e214da99666710
Minh-Tam <dor012299@gmail.com>
2021-09-27 14:02:13 -0400

    updated main up to step 6, #TODO step 2

--- main.py
+++ main.py
@@ -1,34 +1,35 @@
+import numpy as np
 import os
+import os.path as ospath
+from glob import glob
+
 from sklearn import datasets
 from sklearn.feature_extraction.text import CountVectorizer
-import sklearn.model_selection
-import sklearn.naive_bayes as NB
-import glob
+from sklearn.model_selection import train_test_split
+import sklearn.naive_bayes as skNB
 
 #finding BBC subdir in local directory (BBC folder needs to be in project folder COMP472_P1)
 for subdir, dirs, files in os.walk(os.curdir):
     for dir in dirs:
         if dir == "BBC":
-            bbcpath = os.path.join(subdir,dir)
-            #print(bbcpath)
+            bbcpath = ospath.join(subdir,dir)
 
-#step 2
-#TODO insert step 2 code here
+# === STEP 2 ===
 categories = list()
 fcount = [0]
-for path in glob.glob(bbcpath+'\*'):
-    if os.path.isdir(path) and path != bbcpath: #ignore source folder BBC
-        categories.append(os.path.basename(path))
+for path in glob(bbcpath+'\*'):
+    if ospath.isdir(path) and path != bbcpath: #ignore source folder BBC
+        categories.append(ospath.basename(path))
         fcount[0] = 0
-        for file in glob.glob(path+'\*.txt'):
+        for file in glob(path+'\*.txt'):
             fcount[0] = fcount[0] + 1 #fcount[0] serves as our counter
         fcount.append(fcount[0])
 fcount.pop(0) #remove counter fcount[0]
-print(categories)
-print(fcount)
+
+#TODO finish step 2 code here
 
 
-#step 3
+# === STEP 3 ===
 corpus = datasets.load_files(container_path = bbcpath, description = "BBC dataset", encoding = 'latin1', shuffle=False)
 # returns a 'Bunch' with attributes data[], target[], target_names[], DESCR, filenames[]
 #           data[] = content of file as one string
@@ -38,8 +39,20 @@ corpus = datasets.load_files(container_path = bbcpath, description = "BBC datase
 #      filenames[] = file names
 # -> if we need to randomize data, set shuffle to True(default)
 
-#step 4 TODO adapt for step 6
-"""vectorizerinator = CountVectorizer()
+# === STEP 4 ===
+vectorizerinator = CountVectorizer()
 X = vectorizerinator.fit_transform(corpus.data)
-print(vectorizerinator.get_feature_names())
-print(X.toarray())"""
+dictionary = vectorizerinator.get_feature_names()
+distribution = X.toarray() # dist[doc][word] = wordcount
+
+# === STEP 5 ===
+dist_train, dist_test, target_train, target_test = train_test_split(distribution, corpus.target, random_state=None)
+
+# === STEP 6 ===
+mnbc = skNB.MultinomialNB()
+mnbc.fit(dist_train, target_train)
+prediction = mnbc.predict(dist_test)
+answer = target_test
+
+# === STEP 7 ===
+#TODO
