e6e9057f87bad1f145593e5a99338b5804e46599
Minh Bui <55333649+minhbibui@users.noreply.github.com>
2021-09-29 16:21:17 -0400

    adding part 4
    Convert all nominal and ordinal features to numerical data

--- main.py
+++ main.py
@@ -1,12 +1,18 @@
 import numpy as np
 import matplotlib.pyplot as plt
-import pandas
 import sklearn
 import pandas as pd
+
+# -----------
+# PART 2
+# -----------
 
 # read drug200.csv file
 drug200read = pd.read_csv("drug200.csv")
 
+# -----------
+# PART 3
+# -----------
 # storing all the Drug value from Drug colume to the DrugList
 DrugList = drug200read[["Drug"]].values
 
@@ -43,3 +49,29 @@ drugData = [drugCountA,drugCountB,drugCountC,drugCountX,drugCountY]
 drugNames = ["DrugA", "DrugB", "DrugC", "DrugX", "DrugY"]
 plt.bar(drugNames,drugData)
 plt.savefig('drug-distribution.pdf')
+
+# -----------
+# PART 4
+# -----------
+# Convert BP to numerical number
+drug200read["BP"] = pd.Categorical(drug200read["BP"],['LOW','NORMAL','HIGH'],ordered=True)
+drug200read["BP"] = drug200read["BP"].cat.codes
+
+# Convert Cholesterol to numerical number
+drug200read["Cholesterol"] = pd.Categorical(drug200read["Cholesterol"],['LOW','NORMAL','HIGH'],ordered=True)
+drug200read["Cholesterol"] = drug200read["Cholesterol"].cat.codes
+
+# convert Drug to numerical number
+# DrugA = 0, DrugB = 1, DrugC = 2, DrugX = 3, DrugY = 4
+drug200read["Drug"] = pd.Categorical(drug200read["Drug"],['drugA','drugB','drugC','drugX','drugY'],ordered=True)
+drug200read["Drug"] = drug200read["Drug"].cat.codes
+
+# convert Sex to numerical number
+drug200read["Sex"] = pd.Categorical(drug200read["Sex"],['M','F'])
+drug200read["Sex"] = drug200read["Sex"].cat.codes
+drug200read.dropna(axis=0,how='any',inplace=True)
+pd.get_dummies(drug200read)
+
+# -----------
+# PART 5
+# -----------
